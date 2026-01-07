b03cf18425ab314ed517a2c6a764e79f0e6811bb
Iyalini <iyalini20040419@gmail.com>
2026-01-07 08:47:57 +0000

    trying merge again

--- december22.java
+++ december22.java
@@ -2,12 +2,10 @@ public class december22
 {
     public static void main(String args[])
     {
-<<<<<<< HEAD
         System.out.println("Hello World");
         System.out.println("This is Iyalini Rasakumar");
         System.out.println("practicing branch");
         System.out.println("practicing branch 2");
-=======
  
 
 
@@ -16,7 +14,6 @@ public class december22
 
         //this is a one line comment
         
->>>>>>> practice-mer-no-one
     }
     
 }
